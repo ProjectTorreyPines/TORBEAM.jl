ee7b6b25574e074b9bba68c179f840f884d2476c
Severin Denk <denks@fusion.gat.com>
2025-02-12 14:14:04 -0800

    Borked recipe to include current

--- src/OverviewPlot.jl
+++ src/OverviewPlot.jl
@@ -5,45 +5,29 @@ using IMAS
 using Interpolations
 
 
-@recipe function plot_ec_profiles(dd::IMAS.dd)
-    seriestype := :path
-    xlabel --> L"$rho_\mathrm{tor,norm}$"
-    ylabel --> L"$\mathrm{d}P/\mathrm{d}V$ [W M$^{-3}$]"
-    legend --> :topright
-    for ibeam in 1:length(dd.ec_launchers.beam)
+@recipe function plot_ec_profiles(wc1d::IMAS.waves__coherent_wave___profiles_1d; what::Symbol=:power_density)
+     @series begin
+        wc1d, what
+    end
+end
+
+@recipe function plot_ec_profiles(wc::IMAS.waves__coherent_wave)
+    @series begin
+        label := wc.identifier.antenna_name
+        wc.profiles_1d[]
+    end
+end
+
+@recipe function plot_ec_profiles(cwaves::IMAS.IDSvector{<:IMAS.waves__coherent_wave})
+    for cwave in cwaves
         @series begin
-            label := L"\text{Power Beam } $i"
-            yaxis := :left
-            dd.waves.coherent_wave[ibeam].profiles_1d[].grid.rho_tor_norm, dd.waves.coherent_wave[ibeam].profiles_1d[].power_density
+            cwave
         end
-    end
-    @series begin
-        label := L"\text{Total Power}"
-        dd.core_sources.source[source_index].profiles_1d[].grid.rho_tor_norm,
-        dd.core_sources.source[source_index].profiles_1d[].electrons.power_inside
-         
-    end
-    @series begin
-        ylabel := L"$j$ [A M$^{-2}$]"  # Set the right y-axis label
-        nothing, nothing  # Dummy series to attach the label
-    end
-    for ibeam in 1:length(dd.ec_launchers.beam)
-        @series begin
-            label := L"\text{Current Beam } $i"
-            yaxis := :right
-            dd.waves.coherent_wave[ibeam].profiles_1d[].grid.rho_tor_norm, dd.waves.coherent_wave[ibeam].profiles_1d[].current_parallel_density
-        end
-    end
-    
-    @series begin
-        label := L"\text{Total current}"
-        dd.core_sources.source[source_index].profiles_1d[].grid.rho_tor_norm,
-        dd.core_sources.source[source_index].profiles_1d[].current_parallel_inside
     end
 end
 
 
-dd = IMAS.json2imas("samples/D3D_170325_trimmed.json")
+@time dd = IMAS.json2imas("samples/D3D_170325_trimmed.json")
 dd.global_time = 2.0
 torbeam_params = TORBEAM.TorbeamParams()
 TORBEAM.torbeam!(dd, torbeam_params)
