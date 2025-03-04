8e5d714136821aa4fcbe4e063fd937cf62735507
Orso Meneghini <orso82@gmail.com>
2025-03-03 16:44:04 -0800

    Get repo ready for public consumption

---
+++ docs/make.jl
@@ -0,0 +1,43 @@
+using Documenter, TORBEAM
+
+# Call functions
+open(joinpath(@__DIR__, "src/api.md"), "w") do f
+    println(f, "# API Reference\n")
+    for page in keys(TORBEAM.document)
+        if page in [:Expressions, :COCOS] # COCOS is still a WIP
+            continue
+        end
+        println(f, "## $page\n")
+        println(f, "```@docs")
+        for item in TORBEAM.document[page]
+            println(f, "$item")
+        end
+        println(f, "```")
+    end
+end
+
+makedocs(;
+    modules=[TORBEAM],
+    format=Documenter.HTML(;analytics="G-65D8V8C8VQ"),
+    sitename="TORBEAM",
+    checkdocs=:none,
+    pages=["index.md", "api.md", "License" => "license.md", "Notice" => "notice.md"]
+)
+
+# Deploy docs
+# This function deploys the documentation to the gh-pages branch of the repository.
+# The main documentation that will be hosted on
+# https://projecttorreypines.github.io/TORBEAM.jl/stable
+# will be built from latest release tagged with a version number.
+# The development documentation that will be hosted on
+# https://projecttorreypines.github.io/TORBEAM.jl/dev
+# will be built from the latest commit on the chosen devbranch argument below.
+# For testing purposes, the devbranch argument can be set to WIP branch like "docs".
+# While merging with master, the devbranch argument should be set to "master".
+deploydocs(;
+    repo="github.com/ProjectTorreyPines/TORBEAM.jl.git",
+    target="build",
+    branch="gh-pages",
+    devbranch="master",
+    versions=["stable" => "v^", "v#.#"]
+)
--- src/TORBEAM.jl
+++ src/TORBEAM.jl
@@ -431,4 +431,8 @@ using Interpolations
         dd.waves.code.output_flag = zeros(Int64,0) # NO ERROR
 
     end 
+
+    const document = Dict()
+    document[Symbol(@__MODULE__)] = [name for name in Base.names(@__MODULE__; all=false, imported=false) if name != Symbol(@__MODULE__)]
+
 end # module TORBEAM
