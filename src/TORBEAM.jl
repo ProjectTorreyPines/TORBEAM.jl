2050e80c50694466277070a92686313a02986bad
Orso Meneghini <orso82@gmail.com>
2025-03-07 21:15:40 -0800

    mutable struct for TorbeamParams

--- src/TORBEAM.jl
+++ src/TORBEAM.jl
@@ -4,28 +4,29 @@ using Interpolations
 using Plots
 
 
-Base.@kwdef struct TorbeamParams
+Base.@kwdef mutable struct TorbeamParams
     # switches
-    npow::Int = 1
-    ncd::Int = 1
-    ncdroutine::Int = 2
-    nprofv::Int = 50
-    noout::Int = 0
-    nrela::Int = 1
-    nmaxh::Int = 3
-    nabsroutine::Int = 1
-    nastra::Int = 0
-    nprofcalc::Int = 1
-    ncdharm::Int = 1
-    nrel::Int = 0
-    n_ray::Int = 5
-    verbose::Bool = true
-    #Float parameters
-    xrtol::Float64 = 1e-07
-    xatol::Float64 = 1e-07
-    xstep::Float64 = 2.0
-    rhostop::Float64 = 0.96
-    xzsrch::Float64 = 0.0
+    npow::Int = 1             # Power absorption switch (1 = on, 0 = off)
+    ncd::Int = 1              # Current drive calculation switch (1 = on, 0 = off)
+    ncdroutine::Int = 2       # Current drive routine selection (0 = Curba, 1 = Lin-Liu, 2 = Lin-Liu + momentum conservation)
+    nprofv::Int = 50          # Number of radial points for volume profile calculation
+    noout::Int = 0            # Screen output switch (0 = output enabled, 1 = output disabled)
+    nrela::Int = 1            # Relativity consideration in absorption (0 = weakly, 1 = fully relativistic)
+    nmaxh::Int = 3            # Number of harmonics to consider (1 to 5)
+    nabsroutine::Int = 1      # Absorption routine selection (0 = Westerhof, 1 = Farina)
+    nastra::Int = 0           # Definition of driven current density (0 = Lin-Liu, 1 = ASTRA, 2 = JINTRAC)
+    nprofcalc::Int = 1        # Deposition profile calculation method (0 = standard, 1 = Maj method)
+    ncdharm::Int = 1          # Harmonic consideration in current drive efficiency (0 = lowest harmonic only, 1 = includes next harmonic)
+    nrel::Int = 0             # Relativistic mass correction for reflectometry (1 = enabled, 0 = disabled)
+    n_ray::Int = 5            # Number of rays used in beam tracing
+    verbose::Bool = false     # Verbose mode for output debugging (true = enabled, false = disabled)
+
+    # Float parameters
+    xrtol::Float64 = 1e-07    # Required relative error tolerance
+    xatol::Float64 = 1e-07    # Required absolute error tolerance
+    xstep::Float64 = 2.0      # Integration step in vacuum (cm)
+    rhostop::Float64 = 0.96   # Maximum value of the flux coordinate (rho) before stopping
+    xzsrch::Float64 = 0.0     # Vertical position for searching the magnetic axis (default 0 cm)
 end
 
 function torbeam!(dd::IMAS.dd, torbeam_params::TorbeamParams)
